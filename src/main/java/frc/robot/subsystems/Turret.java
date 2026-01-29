fb385fa2535aee9fa687d6b37939ce9698b84a04
Sriram M <srirammartha1@gmail.com>
2026-01-29 17:09:42 -0500

    More cleaning

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -7,7 +7,6 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -19,17 +18,27 @@ import org.littletonrobotics.junction.Logger;
 public class Turret extends SubsystemBase {
   Drive drive;
   Transform3d turretOffset;
+  Transform2d turretTransform2d;
+  Pose2d turretPos;
 
+  //An array for fuel that are in the air
   private final List<Fuel> activeFuel = new ArrayList<>();
+
+  //Hub dimensions on the XY plane
   Translation2d hub = new Translation2d(11.915, 4.035);
+
 
   public Turret(Drive drive, Transform3d turretOffset) {
     this.drive = drive;
     this.turretOffset = turretOffset;
+
+    turretTransform2d = new Transform2d(turretOffset.getX(), turretOffset.getY(), new Rotation2d());
+    turretPos = drive.getPose().plus(turretTransform2d);
   }
 
   @Override
   public void periodic() {
+    //Turret Yaw Visualization
     Logger.recordOutput("ZeroedComponentPoses", new Pose3d[] {new Pose3d()});
     Logger.recordOutput(
         "FinalPoses",
@@ -40,27 +49,25 @@ public class Turret extends SubsystemBase {
                   0,
                   0,
                   trackHub(
-                      drive.getPose(),
-                      new Transform2d(
-                          turretOffset.getX(), turretOffset.getY(), new Rotation2d()))))
+                      drive.getPose())))
         });
 
+    //Removes the fuel once the isDead() function is met, and updates the time for the fuel.
     activeFuel.removeIf(
         fuel -> {
           fuel.update(0.02);
           return fuel.isDead();
         });
-
+    
+    //Records the fuel to AdvantageScope
     Logger.recordOutput(
         "GamePieces/Fuel", activeFuel.stream().map(Fuel::getPose).toArray(Pose3d[]::new));
   }
 
-  public double trackHub(Pose2d robotPose, Transform2d turretOffset) {
-    
-    Pose2d turretPose = robotPose.plus(turretOffset);
-
-    double deltax = hub.getX() - turretPose.getX();
-    double deltay = hub.getY() - turretPose.getY();
+  //Aim to the hub
+  public double trackHub(Pose2d robotPose) {
+    double deltax = hub.getX() - turretPos.getX();
+    double deltay = hub.getY() - turretPos.getY();
     double initTheta;
     // Initial theta
     if (deltax == 0) {
@@ -74,27 +81,10 @@ public class Turret extends SubsystemBase {
             + Math.PI / 2
             + Math.signum(deltax) * Math.PI / 2;
 
-    SmartDashboard.putNumber("Pitch Theta", calcPitch() * 180 / Math.PI);
-
     return theta;
   }
 
-  public void shootBall() {
-    Transform2d leftturretOffset = new Transform2d(-0.17, -0.15, new Rotation2d());
-    Transform2d rightturretOffset = new Transform2d(-0.17, 0.15, new Rotation2d());
-    activeFuel.add(new Fuel(calcPitch(), calcVelocity(), drive.getPose().plus(leftturretOffset)));
-    activeFuel.add(new Fuel(calcPitch(), calcVelocity(), drive.getPose().plus(rightturretOffset)));
-  }
-
-  public Command shootBallCommand() {
-    return runOnce(
-        () -> {
-          shootBall();
-        });
-  }
-
   public double calcVelocity() {
-    Pose2d turretPos = drive.getPose().plus(new Transform2d(turretOffset.getX(), turretOffset.getY(), new Rotation2d()));
     Translation2d hub = new Translation2d(11.915, 4.035);
     double yf = 1.329;
     double xf =
@@ -110,8 +100,6 @@ public class Turret extends SubsystemBase {
   }
 
   public double calcPitch() {
-    Transform2d turretOffset = new Transform2d(-0.17, -0.15, new Rotation2d());
-    Pose2d turretPos = drive.getPose().plus(turretOffset);
     Translation2d hub = new Translation2d(11.915, 4.035);
     double yf = 1.329;
     double xf =
@@ -129,5 +117,16 @@ public class Turret extends SubsystemBase {
     double theta = Math.atan((-B - Math.sqrt(Math.pow(B, 2) - 4 * A * C)) / (2 * A));
 
     return theta;
+  }
+
+  public void shootBall() {
+    activeFuel.add(new Fuel(calcPitch(), calcVelocity(), drive.getPose().plus(turretTransform2d)));
+  }
+
+  public Command shootBallCommand() {
+    return runOnce(
+        () -> {
+          shootBall();
+        });
   }
 }
