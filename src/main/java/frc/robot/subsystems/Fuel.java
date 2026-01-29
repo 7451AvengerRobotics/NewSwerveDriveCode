dcd39f2cf9271354075c32d0086df4042d87a3f3
Sriram M <srirammartha1@gmail.com>
2026-01-29 16:54:24 -0500

    Updates and cleaning

--- src/main/java/frc/robot/subsystems/Fuel.java
+++ src/main/java/frc/robot/subsystems/Fuel.java
@@ -1,13 +1,13 @@
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Meter;
+import java.util.concurrent.locks.Condition;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Fuel {
   double pitch;
@@ -19,35 +19,53 @@ public class Fuel {
   double accel = 9.8;
   double height = 0.5;
   Translation2d hub = new Translation2d(11.915, 4.035);
+  double xChange;
+  double yChange;
+  double yaw;
+  boolean killValue = false;
+  double killTimer;
 
   public Fuel(double pitch, double velocity, Pose2d initialPos) {
     this.pitch = pitch;
     this.velocity = velocity;
     this.initialPos = initialPos;
+
+    xChange = hub.getX() - initialPos.getX();
+    yChange = hub.getY() - initialPos.getY();
+    yaw = Math.atan2(yChange, xChange);
   }
 
   public Pose3d getPose() {
-    double xChange = hub.getX() - initialPos.getX();
-    double yChange = hub.getY() - initialPos.getY();
-    double yaw = Math.atan2(yChange, xChange);
+    return new Pose3d(new Translation3d(getX(), getY(), Math.max(getZ(), 0)), new Rotation3d());
+  }
 
-    double t = time;
+  public double getX() {
+    return initialPos.getX() + velocity * Math.cos(pitch) * Math.cos(yaw) * time;
+  }
 
-    double z = (-0.5 * accel * t * t) + (velocity * Math.sin(pitch) * t) + height;
+  public double getY() {
+    return initialPos.getY() + velocity * Math.cos(pitch) * Math.sin(yaw) * time;
+  }
 
-    double x = initialPos.getX() + velocity * Math.cos(pitch) * Math.cos(yaw) * t;
-
-    double y = initialPos.getY() + velocity * Math.cos(pitch) * Math.sin(yaw) * t;
-
-    return new Pose3d(new Translation3d(x, y, Math.max(z, 0)), new Rotation3d());
+  public double getZ() {
+    return (-0.5 * accel * time * time) + (velocity * Math.sin(pitch) * time) + height;
   }
 
   public void update(double dt) {
     time += dt;
   }
 
-  //Removes the ball after __ seconds
+  // Removes the ball after __ seconds
   public boolean isDead() {
-    return time >= 4;
+    boolean startTimerCondition = (hub.getX() - getX()) < 0.1 && (hub.getX() - getX()) > -0.1 && (hub.getY() - getY()) < 0.1 && (hub.getY() - getY()) > -0.1 && getZ() < 1.829;
+    if (startTimerCondition) {
+      if (!killValue) {
+        killValue = true;
+        killTimer = time;
+      }
+    }
+    SmartDashboard.putNumber("time", killTimer);
+    SmartDashboard.putBoolean("timer condition", startTimerCondition);
+    return killTimer != 0 ? (time - killTimer) > 0.5 : false;
   }
 }
--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -17,87 +18,116 @@ import org.littletonrobotics.junction.Logger;
 
 public class Turret extends SubsystemBase {
   Drive drive;
+  Transform3d turretOffset;
 
   private final List<Fuel> activeFuel = new ArrayList<>();
+  Translation2d hub = new Translation2d(11.915, 4.035);
 
-  public Turret(Drive drive) {
+  public Turret(Drive drive, Transform3d turretOffset) {
     this.drive = drive;
+    this.turretOffset = turretOffset;
   }
 
   @Override
   public void periodic() {
-    Translation3d leftShooterOffset = new Translation3d(-0.17, 0.15, 0.39);
-    Translation3d rightShooterOffset = new Translation3d(-0.17, -0.15, 0.39);
     Logger.recordOutput("ZeroedComponentPoses", new Pose3d[] {new Pose3d()});
     Logger.recordOutput(
         "FinalPoses",
         new Pose3d[] {
           new Pose3d(
-              leftShooterOffset,
+              turretOffset.getTranslation(),
               new Rotation3d(
                   0,
                   0,
                   trackHub(
                       drive.getPose(),
                       new Transform2d(
-                          leftShooterOffset.getX(), leftShooterOffset.getY(), new Rotation2d())))),
-          new Pose3d(
-              rightShooterOffset,
-              new Rotation3d(
-                  0,
-                  0,
-                  trackHub(
-                      drive.getPose(),
-                      new Transform2d(
-                          rightShooterOffset.getX(), rightShooterOffset.getY(), new Rotation2d()))))
+                          turretOffset.getX(), turretOffset.getY(), new Rotation2d()))))
         });
 
-    activeFuel.removeIf(fuel -> {
-        fuel.update(0.02);
-        return fuel.isDead();
-    });
+    activeFuel.removeIf(
+        fuel -> {
+          fuel.update(0.02);
+          return fuel.isDead();
+        });
 
     Logger.recordOutput(
         "GamePieces/Fuel", activeFuel.stream().map(Fuel::getPose).toArray(Pose3d[]::new));
   }
 
   public double trackHub(Pose2d robotPose, Transform2d turretOffset) {
-    Translation2d hub = new Translation2d(11.915, 4.035);
+    
     Pose2d turretPose = robotPose.plus(turretOffset);
 
     double deltax = hub.getX() - turretPose.getX();
     double deltay = hub.getY() - turretPose.getY();
-
+    double initTheta;
     // Initial theta
-    double initTheta = Math.atan(deltay / deltax);
+    if (deltax == 0) {
+      initTheta = 0;
+    } else {
+      initTheta = Math.atan(deltay / deltax);
+    }
 
     double theta =
         (initTheta - robotPose.getRotation().getRadians())
             + Math.PI / 2
             + Math.signum(deltax) * Math.PI / 2;
 
-    SmartDashboard.putNumber("Turret Theta", theta * 180 / Math.PI);
+    SmartDashboard.putNumber("Pitch Theta", calcPitch() * 180 / Math.PI);
 
     return theta;
   }
 
   public void shootBall() {
-    Transform2d turretOffset = new Transform2d(-0.17, -0.15, new Rotation2d());
-    activeFuel.add(new Fuel(calcPitch(), calcVelocity(), drive.getPose().plus(turretOffset)));
+    Transform2d leftturretOffset = new Transform2d(-0.17, -0.15, new Rotation2d());
+    Transform2d rightturretOffset = new Transform2d(-0.17, 0.15, new Rotation2d());
+    activeFuel.add(new Fuel(calcPitch(), calcVelocity(), drive.getPose().plus(leftturretOffset)));
+    activeFuel.add(new Fuel(calcPitch(), calcVelocity(), drive.getPose().plus(rightturretOffset)));
   }
 
   public Command shootBallCommand() {
     return runOnce(
-      () -> {
-        shootBall();
-      });
+        () -> {
+          shootBall();
+        });
   }
 
   public double calcVelocity() {
-    return 6;
+    Pose2d turretPos = drive.getPose().plus(new Transform2d(turretOffset.getX(), turretOffset.getY(), new Rotation2d()));
+    Translation2d hub = new Translation2d(11.915, 4.035);
+    double yf = 1.329;
+    double xf =
+        Math.sqrt(
+            Math.pow((hub.getX() - turretPos.getX()), 2)
+                + Math.pow((hub.getY() - turretPos.getY()), 2));
+    double g = 9.8;
+    double vel = Math.sqrt(
+      g*yf + g*Math.sqrt(Math.pow(xf, 2) + Math.pow(yf, 2))
+    ) + 0.5;
+
+    return vel;
   }
 
   public double calcPitch() {
-    return Math.PI / 4;
+    Transform2d turretOffset = new Transform2d(-0.17, -0.15, new Rotation2d());
+    Pose2d turretPos = drive.getPose().plus(turretOffset);
+    Translation2d hub = new Translation2d(11.915, 4.035);
+    double yf = 1.329;
+    double xf =
+        Math.sqrt(
+            Math.pow((hub.getX() - turretPos.getX()), 2)
+                + Math.pow((hub.getY() - turretPos.getY()), 2));
+    double v = calcVelocity();
+    double g = 9.8;
+    double a = -g;
+
+    double A = a * Math.pow(xf, 2) / (2 * Math.pow(v, 2));
+    double B = xf;
+    double C = A - yf;
+
+    double theta = Math.atan((-B - Math.sqrt(Math.pow(B, 2) - 4 * A * C)) / (2 * A));
+
+    return theta;
   }
 }
